aa85b7b26fbb1e44fa9a8df991f2b8f530e7db24
shaileshkumar36git config --global user.email shailesraj7857@gmail.com <shaileshraj7857@gmail.com>
2025-04-27 21:12:16 +0530

    Call by value in java

---
+++ Class2Function/Class1.java
@@ -0,0 +1,23 @@
+package JavaClass.Class2Function;
+
+import java.util.*;
+// Call by value 
+public class Class1 {
+    public static void CallbyValue(int arr[] ,int a){
+        for(int i=0;i<arr.length;i++){
+            arr[i] *= 2;
+            a *= 2;
+        }
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int arr[] = {3,4,5,6,7};
+        CallbyValue(arr,a);
+        for(int i=0;i<arr.length;i++){
+            System.out.println("arr changed value is :   "    + arr[i]);
+            
+        }System.out.println("a not changed value : "+a);
+    }
+}
