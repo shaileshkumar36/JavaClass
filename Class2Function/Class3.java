210d0d3b594bc7d928439ff219a6d7da9da4fcaa
shaileshkumar36git config --global user.email shailesraj7857@gmail.com <shaileshraj7857@gmail.com>
2025-04-27 21:49:33 +0530

    Block element

---
+++ Class2Function/Class3.java
@@ -0,0 +1,17 @@
+package JavaClass.Class2Function;
+
+import java.util.*;
+public class Class3 {
+    public static void main(String[] args) {
+        int a = 8;
+
+        {
+            int b = 2;
+            System.out.println(b);
+            System.out.println(a);
+
+        }
+        //System.out.println(b);  //we cant acction the file there becouse this element are in side the chile block 
+        //elsement 
+    }
+}
